8ac4cf77810639705c1f9e99e077e447cfdb15c0
luiza fsc <luizascrs@gmail.com>
2023-07-25 10:50:11 -0300

    adicionando praia e ifc

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/cool', function(req, res, next) {
+router.get('/cool', function (req, res, next) {
   res.render('index', { title: 'you are so cool' });
+});
+
+router.get('/ifc', function (req, res, next) {
+  res.render('index', { title: 'bem-vindo ao ifc' });
+});
+
+router.get('/cool/beach', function (req, res, next) {
+  res.render('index', { title: 'adoro praia' });
 });
 
 module.exports = router;
